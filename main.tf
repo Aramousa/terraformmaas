1c3463f280469f180f94a78306120342feb0d2da
delphi5 <arash_mousapour@msn.com>
2023-12-27 10:17:29 +0330

    create infrastructure

--- main.tf
+++ main.tf
@@ -13,3 +13,38 @@ provider "maas" {
   api_url     = "http://10.10.10.26:5240/MAAS"
 }
 
+resource "maas_space" "tf_space" {
+  name = "tf-space"
+}
+
+resource "maas_fabric" "tf_fabric" {
+  name = "tf-fabric"
+}
+
+resource "maas_vlan" "tf_vlan" {
+  fabric = maas_fabric.tf_fabric.id
+  vid = 10
+  name = "tf-vlan10"
+  space = maas_space.tf_space.name
+}
+resource "maas_subnet" "tf_subnet" {
+  cidr = "10.10.12.0/24"
+  fabric = maas_fabric.tf_fabric.id
+  vlan = maas_vlan.tf_vlan.vid
+  name = "tf_subnet"
+  gateway_ip = "10.10.12.1"
+  dns_servers = [
+    "1.1.1.1",
+    "8.8.8.8",
+  ]
+  ip_ranges {
+    type = "reserved"
+    start_ip = "10.10.12.10"
+    end_ip = "10.10.12.80"
+  }
+  ip_ranges {
+    type = "dynamic"
+    start_ip = "10.10.12.150"
+    end_ip = "10.10.12.220"
+  }
+}
